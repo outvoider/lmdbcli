45580f4e4d632532b74e0a8e70123640b567277c
outvoider <outvoider@yahoo.com>
2016-08-26 10:39:20 -0400

    Version 1.1. Improved lmdb client.

--- lmdbcli/LmdbClient.hpp
+++ lmdbcli/LmdbClient.hpp
@@ -24,7 +24,7 @@ public:
 
 		if (!TryGet(path, key, value))
 		{
-			throw exception("ClientLmdb::Get(): failed");
+			throw exception("ClientLmdb::Get(): failed to get the value of a key");
 		}
 
 		return value;
@@ -50,21 +50,55 @@ public:
 	{
 		value = "";
 
-		int rc;
-		MDB_env *env;
-		MDB_dbi dbi;
+		int rc = 0;
+		MDB_env *env = NULL;
+		MDB_dbi dbi = 0;
+		MDB_txn *txn = NULL;
 		MDB_val key_p, data_p;
-		MDB_txn *txn;
+		key_p.mv_data = NULL;
+		key_p.mv_size = 0;
+		data_p.mv_data = NULL;
+		data_p.mv_size = 0;
 
 		rc = mdb_env_create(&env);
-		rc |= mdb_env_open(env, path.c_str(), 0, 0664);
-		rc |= mdb_txn_begin(env, NULL, 0, &txn);
-		rc |= mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::TryGet(): failed to create an environment");
+		}
+
+		rc = mdb_env_open(env, path.c_str(), 0, 0664);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::TryGet(): failed to open an environment");
+		}
+
+		rc = mdb_txn_begin(env, NULL, 0, &txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::TryGet(): failed to begin a transaction");
+		}
+
+		rc = mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::TryGet(): failed to open a database");
+		}
 
 		key_p.mv_data = (void*)key.data();
 		key_p.mv_size = strlen((const char*)key_p.mv_data);
 
-		rc |= mdb_get(txn, dbi, &key_p, &data_p);
+		rc = mdb_get(txn, dbi, &key_p, &data_p);
 
 		if (rc == 0)
 		{
@@ -78,7 +112,6 @@ public:
 		return (rc == 0);
 	}
 
-
 	static
 		void
 		Set(
@@ -86,31 +119,78 @@ public:
 		, const string &key
 		, const string &value)
 	{
-		int rc;
-		MDB_env *env;
-		MDB_dbi dbi;
+		int rc = 0;
+		MDB_env *env = NULL;
+		MDB_dbi dbi = 0;
+		MDB_txn *txn = NULL;
 		MDB_val key_p, data_p;
-		MDB_txn *txn;
+		key_p.mv_data = NULL;
+		key_p.mv_size = 0;
+		data_p.mv_data = NULL;
+		data_p.mv_size = 0;
 
 		rc = mdb_env_create(&env);
-		rc |= mdb_env_open(env, path.c_str(), 0, 0664);
-		rc |= mdb_txn_begin(env, NULL, 0, &txn);
-		rc |= mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to create an environment");
+		}
+
+		rc = mdb_env_open(env, path.c_str(), 0, 0664);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to open an environment");
+		}
+
+		rc = mdb_txn_begin(env, NULL, 0, &txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to begin a transaction");
+		}
+
+		rc = mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to open a database");
+		}
 
 		key_p.mv_data = (void*)key.data();
 		key_p.mv_size = strlen((const char*)key_p.mv_data);
 		data_p.mv_data = (void*)value.data();
 		data_p.mv_size = strlen((const char*)data_p.mv_data);
 
-		rc |= mdb_put(txn, dbi, &key_p, &data_p, 0);
-		rc |= mdb_txn_commit(txn);
-		mdb_close(env, dbi);
-		mdb_env_close(env);
+		rc = mdb_put(txn, dbi, &key_p, &data_p, 0);
 
 		if (rc != 0)
 		{
-			throw exception("ClientLmdb::Set(): failed");
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to set the value of a key");
 		}
+
+		rc = mdb_txn_commit(txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to commit a transaction");
+		}
+
+		mdb_close(env, dbi);
+		mdb_env_close(env);
 	}
 
 	static
@@ -119,38 +199,74 @@ public:
 		const string &path
 		, const string &key)
 	{
-		int rc;
-		MDB_env *env;
-		MDB_dbi dbi;
+		int rc = 0;
+		MDB_env *env = NULL;
+		MDB_dbi dbi = 0;
+		MDB_txn *txn = NULL;
 		MDB_val key_p;
-		MDB_txn *txn;
+		key_p.mv_data = NULL;
+		key_p.mv_size = 0;
 
 		rc = mdb_env_create(&env);
-		rc |= mdb_env_open(env, path.c_str(), 0, 0664);
-		rc |= mdb_txn_begin(env, NULL, 0, &txn);
-		rc |= mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to create an environment");
+		}
+
+		rc = mdb_env_open(env, path.c_str(), 0, 0664);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to open an environment");
+		}
+
+		rc = mdb_txn_begin(env, NULL, 0, &txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to begin a transaction");
+		}
+
+		rc = mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to open a database");
+		}
 
 		key_p.mv_data = (void*)key.data();
 		key_p.mv_size = strlen((const char*)key_p.mv_data);
 
-		rc |= mdb_del(txn, dbi, &key_p, NULL);
+		rc = mdb_del(txn, dbi, &key_p, NULL);
 
-		if (rc == 0)
-		{
-			rc |= mdb_txn_commit(txn);
-		}
-		else
+		if (rc != 0)
 		{
 			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to remove a key");
+		}
+
+		rc = mdb_txn_commit(txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to commit a transaction");
 		}
 
 		mdb_close(env, dbi);
 		mdb_env_close(env);
-
-		if (rc != 0)
-		{
-			throw exception("ClientLmdb::Remove(): failed");
-		}
 	}
 
 	static
@@ -159,27 +275,71 @@ public:
 		const string &path
 		, function<void(const string &key, const string &value)> OnKyeValue)
 	{
-		int rc;
-		MDB_env *env;
-		MDB_dbi dbi;
+		int rc = 0;
+		MDB_env *env = NULL;
+		MDB_dbi dbi = 0;
+		MDB_txn *txn = NULL;
 		MDB_val key_p, data_p;
-		MDB_txn *txn;
-		MDB_cursor *cursor;
+		key_p.mv_data = NULL;
+		key_p.mv_size = 0;
+		data_p.mv_data = NULL;
+		data_p.mv_size = 0;
+		MDB_cursor *cursor = NULL;
 
 		rc = mdb_env_create(&env);
-		rc |= mdb_env_open(env, path.c_str(), 0, 0664);
-		rc |= mdb_txn_begin(env, NULL, 0, &txn);
-		rc |= mdb_open(txn, NULL, 0, &dbi);
-		rc |= mdb_cursor_open(txn, dbi, &cursor);
 
-		while (0 == (rc |= mdb_cursor_get(cursor, &key_p, &data_p, MDB_NEXT)))
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to create an environment");
+		}
+
+		rc = mdb_env_open(env, path.c_str(), 0, 0664);
+
+		if (rc != 0)
+		{
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Set(): failed to open an environment");
+		}
+
+		rc = mdb_txn_begin(env, NULL, 0, &txn);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to begin a transaction");
+		}
+
+		rc = mdb_open(txn, NULL, 0, &dbi);
+
+		if (rc != 0)
+		{
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to open a database");
+		}
+
+		rc = mdb_cursor_open(txn, dbi, &cursor);
+
+		if (rc != 0)
+		{
+			mdb_cursor_close(cursor);
+			mdb_txn_abort(txn);
+			mdb_close(env, dbi);
+			mdb_env_close(env);
+			throw exception("ClientLmdb::Remove(): failed to open a cursor");
+		}
+
+		while (0 == (rc = mdb_cursor_get(cursor, &key_p, &data_p, MDB_NEXT)))
 		{
 			OnKyeValue(string((const char*)key_p.mv_data, key_p.mv_size), string((const char*)data_p.mv_data, data_p.mv_size));
 		}
 
 		mdb_cursor_close(cursor);
-		mdb_close(env, dbi);
 		mdb_txn_abort(txn);
+		mdb_close(env, dbi);
 		mdb_env_close(env);
 	}
 };
